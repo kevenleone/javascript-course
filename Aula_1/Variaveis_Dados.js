b713c22b035283e993fb82f31dbabc3f29dd415a
Keven <keven.santos.sz@gmail.com>
2020-10-31 17:02:33 -0300

    aula 4

--- Aula_1/Variaveis_Dados.js
+++ Aula_1/Variaveis_Dados.js
@@ -21,9 +21,9 @@ const alunos = [aluno, aluno2]
 
 const posts = [ // Object -> Array
   {
-    userId: 1,
-    id: 1,
-    title:
+    "userId": 1,
+    "id": 1,
+    "title":
       "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
     body:
       "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
